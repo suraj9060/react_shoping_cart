da48bea4a31157d6c090f81ed0d6de447db1377c
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-05-30 00:01:41 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter className="App">
       <Header />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Navbar, Container, FormControl, Nav, Dropdown, Badge } from "react-bootstrap"
 import { FaShoppingCart } from "react-icons/fa";
+import {Link} from "react-router-dom"
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Link to="/" >Brand link</Link>
+        </Navbar.Brand>
         <Navbar.Text className="search">
           <FormControl
             style={{ width: 500 }}
@@ -16,13 +19,13 @@ const Header = () => {
         </Navbar.Text>
         <Nav>
           <Dropdown alignRight>
-            <Dropdown.Toggle variant='success'>
-              <FaShoppingCart color='white' fontSize="25px" />
-              <Badge>{ 5}</Badge>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{5}</Badge>
             </Dropdown.Toggle>
 
             <Dropdown.Menu style={{ minWidth: 370 }}>
-              <span style={{padding:10}}>Cart is Empty</span>
+              <span style={{ padding: 10 }}>Cart is Empty</span>
             </Dropdown.Menu>
           </Dropdown>
         </Nav>
